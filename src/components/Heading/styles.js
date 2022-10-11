e0fc587f22d1cc659ed8a84a25a2d94763ec3fbb
Yan <yangabriel2012@gmail.com>
2022-10-11 08:40:59 -0300

    implemntação das fonts novas

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -1,7 +1,33 @@
 import styled, { css } from "styled-components";
 import { theme } from "../../styles/theme";
+const titleSize = {
+  small: (theme) => css`
+    font-size: ${theme.font.sizes.medium};
+  `,
+  medium: (theme) => css`
+    font-size: ${theme.font.sizes.large};
+  `,
+  big: (theme) => css`
+    font-size: ${theme.font.sizes.xlarge};
+  `,
+  huge: (theme) => css`
+    font-size: ${theme.font.sizes.xhuge};
+    ${mediaFont(theme)}
+  `,
+};
+const titleCase = (uppercase) => css`
+  text-transform: ${uppercase ? "uppercase" : "none"};
+`;
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.font.sizes.xlarge};
+  }
+`;
+
 export const Title = styled.h1`
-  ${({ theme, colorDark }) => css`
+  ${({ theme, colorDark, size, uppercase }) => css`
     color: ${colorDark ? theme.colors.white : theme.colors.primaryColor};
+    ${titleSize[size](theme)};
+    ${titleCase(uppercase)}
   `}
 `;
