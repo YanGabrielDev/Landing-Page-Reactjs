d0b340de2d513fd5b71f48b78a47cc739ab8155e
Yan <yangabriel2012@gmail.com>
2022-10-11 08:42:11 -0300

    impletação dos objetos com as fonts

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,8 +6,21 @@ export const theme = {
   },
   font: {
     family: {
-      default: "'Inter', sans-serif",
-      secondary: "'Ubuntu', sans-serif",
+      default: "font-family: 'Lato', sans-serif",
+      secondary: "font-family: 'Lato', sans-serif",
     },
+    sizes: {
+      xsmall: "8rem",
+      small: "1.6rem",
+      medium: "2.4rem",
+      large: "3.2rem",
+      xlarge: "4.0rem",
+      xxlarge: "4.8rem",
+      huge: "5.6rem",
+      xhuge: "6.4rem",
+    },
+  },
+  media: {
+    lteMedium: "(max-width:768px)",
   },
 };
