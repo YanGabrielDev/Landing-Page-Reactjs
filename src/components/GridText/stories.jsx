3e52b7e528775159f7a40df8244ef67c31b953c3
yangabrp <yg@forpeople.io>
2022-10-17 16:04:45 -0300

    update working

---
+++ src/components/GridText/stories.jsx
@@ -0,0 +1,16 @@
+// import { GridText } from ".";
+// import mock from "./mock";
+
+// export default {
+//   title: "GridText",
+//   component: GridText,
+//   args: ,
+// };
+
+// export const Template = (args) => {
+//   return (
+//     <div>
+//       <GridText {...args} />
+//     </div>
+//   );
+// };
