9b33a03c07ab811a8caea78b42a564edd5faa753
yangabrp <yg@forpeople.io>
2022-10-18 15:31:10 -0300

    fix: alteção na tag usada no Container do TextComponent

--- src/components/TextComponent/styles.js
+++ src/components/TextComponent/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { css } from "styled-components";
-export const Container = styled.p`
+export const Container = styled.div`
   ${({ theme }) => css`
     font-size: ${theme.font.sizes.medium};
   `}
