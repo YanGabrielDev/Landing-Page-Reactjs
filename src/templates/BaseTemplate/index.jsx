1eadc653fef655bc4acfcac885b74be643ca81de
yangabrp <yg@forpeople.io>
2022-10-25 17:53:32 -0300

    wp

--- src/templates/BaseTemplate/index.jsx
+++ src/templates/BaseTemplate/index.jsx
@@ -11,7 +11,7 @@ export const Base = ({ links = [], logoData, footerHtml, children }) => {
       <Menu links={links} logoData={logoData} />
       <Styled.Container>
         {children}
-        <Footer html={footerHtml} />
+        <Footer footerHtml={footerHtml} />
       </Styled.Container>
       <GoTop />
     </>
