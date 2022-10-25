41b6263fb0ed7336b4d5dedeb5760c5a7114599d
Yan <yangabriel2012@gmail.com>
2022-10-25 08:46:18 -0300

    fix: alteração no teste dos dados

--- src/api/map-data-test.js
+++ src/api/map-data-test.js
@@ -1,29 +1,23 @@
-import mapData from "./map-data";
+import { mapData } from "./map-data";
+
 describe("map-data", () => {
   it("should map data even if there is no data", () => {
-    const pageData = mapData();
-    expect(pageData.footerHtml).toBe("");
-    expect(pageData.slug).toBe("");
-    expect(pageData.title).toBe("");
-    expect(pageData.sections).toEqual([]);
-    expect(pageData.menu).toEqual({});
+    const pagesData = mapData()[0];
+    expect(pagesData.footerHtml).toBe("");
+    expect(pagesData.slug).toBe("");
+    expect(pagesData.title).toBe("");
   });
-});
 
-it("should map data even if data is present", () => {
-  const pageData = mapData([
-    {
-      footer_text: "<p>Hey</p>",
-      slug: "slug",
-      title: "title",
-      sections: [1, 2, 3, 4],
-      footer_text: "<p>Hey</p>",
-      menu: { a: "b" },
-    },
-  ])[0];
-  expect(pageData.footerHtml).toBe("<p>Hey</p>");
-  expect(pageData.slug).toBe("");
-  expect(pageData.title).toBe("");
-  expect(pageData.sections).toEqual([1, 2, 3, 4]);
-  expect(pageData.menu).toEqual({ a: "b" });
+  it("should map data if there are data", () => {
+    const pagesData = mapData([
+      {
+        footer_text: "<p>Hey</p>",
+        slug: "slug",
+        title: "title",
+      },
+    ])[0];
+    expect(pagesData.footerHtml).toBe("<p>Hey</p>");
+    expect(pagesData.slug).toBe("slug");
+    expect(pagesData.title).toBe("title");
+  });
 });
